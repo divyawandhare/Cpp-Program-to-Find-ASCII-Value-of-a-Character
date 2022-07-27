99434348b0760f7d1e6031afda9887cdb09e09ed
divyawandhare <68555772+divyawandhare@users.noreply.github.com>
2022-07-27 18:10:11 +0530

    Create Cpp22.cpp

---
+++ Cpp22.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+ char c;
+ cout << "Enter a character: ";
+ cin >> c;
+ cout << "ASCII Value of " << c << " is " << int(c);
+ return 0;
+}
+
+Output
+
+
+Enter a character: p
+ASCII Value of p is 112
